3581637161fb8c3f73b0a4e995083f50f1f0a216
KauBoui <errolbucy@gmail.com>
2023-04-12 11:38:46 -0700

    Added concatenation & closed check

--- src/ComplexPaths.jl
+++ src/ComplexPaths.jl
@@ -7,16 +7,16 @@ export AbstractPath, Path, CirclePath, pointsonpath
 
     # Feilds
     - `parameterization::Function`: A parametric function of a complex path.
-    - `start::Complex`: Initial value for the parametric function.
-    - `end::Complex`: Final value for the parametric function.
+    - `start::Number`: Initial value for the parametric function.
+    - `end::Number`: Final value for the parametric function.
 """
 
 abstract type AbstractPath end
 
 struct Path <: AbstractPath
     parameterization::Function
-    start::Complex
-    ending::Complex
+    start::Number
+    ending::Number
 end
 
 """
@@ -35,6 +35,29 @@ function pointsonpath(P::AbstractPath, n::Integer)::Vector
     @assert n > 0 "n must be a positive non-zero integer"
     return [P.parameterization(t) for t in range(P.start, P.ending, length=n)]
 end 
+
+"""
+    *(α::AbstractPath, β::AbstractPath)
+
+    Concatenate two paths together
+"""
+function *(α::AbstractPath, β::AbstractPath)
+    @assert α.ending == β.start "Paths must meet at endpoint."
+    α_par = α.parameterization
+    β_par = β.parameterization
+    new_par = t -> t ≤ α.ending ? α.parameterization(t) : β.parameterization(t)
+    return Path(new_par, α.start, β.ending)
+end
+
+"""
+    isclosed(γ::AbstractPath)::Bool
+
+    Check if `γ` is a closed Path. i.e. the start and end points are the same (within a tolerance for floating point nonsense).
+
+"""
+function isclosed(γ::AbstractPath)::Bool
+    return γ.parameterization(γ.start) ≈ γ.parameterization(γ.ending) atol=0.0001
+end
 
 end # module ComplexPaths
 
